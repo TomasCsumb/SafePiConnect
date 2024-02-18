8b75075a6135ce3034f51a0fce124bbc8ec0b7b9
tomasdiazwahl <tdiazwahl@csumb.edu>
2024-02-17 23:46:59 -0800

    ITS WORRRKING

--- app/src/main/java/com/example/safepiconnect/DeviceActivity.kt
+++ app/src/main/java/com/example/safepiconnect/DeviceActivity.kt
@@ -1,14 +1,21 @@
 package com.example.safepiconnect
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.safepiconnect.databinding.ActivityDeviceBinding
 import kotlinx.coroutines.launch
+import no.nordicsemi.android.common.core.DataByteArray
 import no.nordicsemi.android.kotlin.ble.client.main.callback.ClientBleGatt
 import no.nordicsemi.android.kotlin.ble.core.ServerDevice
+import no.nordicsemi.android.kotlin.ble.core.data.BleWriteType
+import java.util.UUID
 
 class DeviceActivity : AppCompatActivity() {
 
@@ -27,18 +34,44 @@ class DeviceActivity : AppCompatActivity() {
             val name = it.name ?: "Unknown Device"
             val address = it.address
 
+
             // Add text views to your table rows
             addDeviceDetailToRow(binding.row0, "Device Name: ", name)
             addDeviceDetailToRow(binding.row1, "MAC Address: ", address)
-//            connectToDevice(address, clientDevice)
+            connectToDevice(address)
         }
 
     }
 
-    private fun connectToDevice(address: String, clientDevice: ClientBleGatt) {
+    private fun connectToDevice(address: String) {
         lifecycleScope.launch {
-            clientDevice.reconnect()
-//           val connection =
+            if (ActivityCompat.checkSelfPermission(
+                    this@DeviceActivity,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+//                return
+            }
+
+            val connection = ClientBleGatt.connect(this@DeviceActivity, address, this)
+            val services = connection.discoverServices()
+            val service = services.findService(UUID.fromString("A07498CA-AD5B-474E-940D-16F1FBE7E8CD"))!!
+            val readCharacteristic = service?.findCharacteristic(UUID.fromString("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"))!!
+            val message = readCharacteristic.read().value?.toString(Charsets.UTF_8) ?: ""
+            val writeCharacteristic = service?.findCharacteristic(UUID.fromString("52FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"))!!
+            val secretStringMessage = "This is my message"
+            val secretMessage = secretStringMessage.toByteArray(Charsets.UTF_8)
+            val dataByteArray = DataByteArray(secretMessage)
+            writeCharacteristic.write(dataByteArray, BleWriteType.DEFAULT)
+
+            Log.d("DeviceActivity", message)
         }
     }
 
