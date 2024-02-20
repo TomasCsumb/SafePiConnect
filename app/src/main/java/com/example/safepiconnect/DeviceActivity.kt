a895b6564875f4a6fa37c9ffef35a7b6f726e356
Elliot Shabram <elliotshabram@gmail.com>
2024-02-19 23:41:06 -0800

    nothing

--- app/src/main/java/com/example/safepiconnect/DeviceActivity.kt
+++ app/src/main/java/com/example/safepiconnect/DeviceActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.lifecycle.lifecycleScope
 import com.example.safepiconnect.databinding.ActivityDeviceBinding
 import kotlinx.coroutines.launch
@@ -15,9 +16,6 @@ import java.util.UUID
 class DeviceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDeviceBinding
 //    private lateinit var clientDevice: ClientBleGatt
-//    private val SERVICE_ID = UUID.fromString("A07498CA-AD5B-474E-940D-16F1FBE7E8CD")
-//    private val READ_CHARACTERISTIC_UUID = UUID.fromString("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
-//    private val WRITE_CHARACTERISTIC_UUID = UUID.fromString("52FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +29,15 @@ class DeviceActivity : AppCompatActivity() {
             // Assuming BleDevice has methods getName() and getAddress()
             val name = it.name ?: "Unknown Device"
             val address = it.address
-
+            val hasName = it.hasName
+            val bonded = it.isBonded
 
             // Add text views to your table rows
             addDeviceDetailToRow(binding.row0, "Device Name: ", name)
             addDeviceDetailToRow(binding.row1, "MAC Address: ", address)
+            addDeviceDetailToRow(binding.row2, "Bonded: ", bonded.toString())
+
+
 
             // Create ble manager for connecting. We must call the methods from within so as to ensure
             // that the connection is complete before a read or write. This must be done because blocking
